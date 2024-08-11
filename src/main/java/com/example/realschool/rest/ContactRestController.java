a7957987ea0f978f36ea2de405693f80df332178
Jahid Aneek <jahid.onic162@gmail.com>
2024-08-11 13:50:57 +0600

    Added Rest Services-Create

---
+++ src/main/java/com/example/realschool/rest/ContactRestController.java
@@ -0,0 +1,50 @@
+package com.example.realschool.rest;
+
+import com.example.realschool.model.Contact;
+import com.example.realschool.model.Response;
+import com.example.realschool.repository.ContactRepository;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/contact")
+public class ContactRestController {
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    @GetMapping("/getMessagesByStatus")
+    public List<Contact> getMessagesByStatus(@RequestParam(name = "status") String status) {
+        return contactRepository.findByStatus(status);
+    }
+
+    @GetMapping("/getAllMsgsByStatus")
+    public List<Contact> getAllMsgsByStatus(@RequestBody Contact contact) {
+        if (contact.getStatus() != null) {
+            return contactRepository.findByStatus(contact.getStatus());
+        } else {
+            return List.of();
+        }
+    }
+
+    @PostMapping("/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestHeader("invocationFrom") String invocationFrom,
+                                            @Valid @RequestBody Contact contact) {
+        log.info("saveMsg invocationFrom: {}, contact: {}", invocationFrom, contact);
+        contactRepository.save(contact);
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message saved successfully");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("isMsgSaved", "true")
+                .body(response);
+    }
+}
