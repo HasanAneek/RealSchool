50066c5fbbc3106932ccdc972ca77bd332e44d91
Jahid Aneek <jahid.onic162@gmail.com>
2024-08-12 01:03:39 +0600

    Added REST api, for ContactRestController class

--- src/main/java/com/example/realschool/controller/GlobalExceptionController.java
+++ src/main/java/com/example/realschool/controller/GlobalExceptionController.java
@@ -1,15 +1,17 @@
 package com.example.realschool.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionController {
 
     @ExceptionHandler(Exception.class)
     public ModelAndView exceptionHandler(Exception e) {
-        String errorMsg = null;
+        String errorMsg;
         ModelAndView errorPage = new ModelAndView();
         errorPage.setViewName("error");
         if (e.getMessage() != null) {
--- src/main/java/com/example/realschool/rest/ContactRestController.java
+++ src/main/java/com/example/realschool/rest/ContactRestController.java
@@ -1,16 +1,20 @@
 package com.example.realschool.rest;
 
+import com.example.realschool.constants.RealSchoolConstants;
 import com.example.realschool.model.Contact;
 import com.example.realschool.model.Response;
 import com.example.realschool.repository.ContactRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -45,6 +49,43 @@ public class ContactRestController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .header("isMsgSaved", "true")
+                .body(response);
+    }
+
+    @DeleteMapping("/deleteMsg")
+    public ResponseEntity<Response> deleteMsg(RequestEntity<Contact> requestEntity) {
+        HttpHeaders headers = requestEntity.getHeaders();
+        headers.forEach((key, value) ->
+                log.info("key: {}, value: {}", key, value));
+        Contact contact = requestEntity.getBody();
+        assert contact != null;
+        contactRepository.deleteById(contact.getContactId());
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message deleted successfully");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+
+    @PatchMapping("/closeMsg")
+    public ResponseEntity<Response> closeMsg(@RequestBody Contact contactReq) {
+        Response response = new Response();
+        Optional<Contact> contact = contactRepository.findById(contactReq.getContactId());
+        if (contact.isPresent()) {
+            contact.get().setStatus(RealSchoolConstants.CLOSE);
+            contactRepository.save(contact.get());
+        } else {
+            response.setStatusCode("404");
+            response.setStatusMsg("Contact not found");
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+        response.setStatusCode("200");
+        response.setStatusMsg("Message closed successfully");
+        return ResponseEntity
+                .status(HttpStatus.OK)
                 .body(response);
     }
 }
